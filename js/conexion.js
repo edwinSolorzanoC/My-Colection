bceab2b3ffc53e9ec931d0c25df4513650051f42
edwinSolorzanoC <edwinuni26@gmail.com>
2024-05-22 15:44:33 -0600

    actualizacion grafica

--- js/conexion.js
+++ js/conexion.js
@@ -2,19 +2,25 @@ let mysql = require("mysql");
 
 
 let conexion = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "localhost",
     database: "dbmycolection",
-    user: "edwin",
+    user: "root",
     password: "patodonal26"
 });
+ 
+// conexion.query("SELECT * FROM tb_usuario", function(error, result, fields){
+//     if(error) throw error
+//     console.log(result)
+    
+//     })
 
- conexion.connect(function(err){
+  conexion.connect(function(err){
      if(err){
-         throw err;
-     }else{
-         console.log("conexion exitosa");
-     }
- });
+        throw err;
+    }else{
+          console.log("conexion exitosa");
+      }
+  });
 
 //la conexion esta mal, da error. Probar en terminal de vs con, node javascript/conexion.js
 //creo que el error es de la linea 4 a las 11, pero mejjor ver video o buscar en chat....
