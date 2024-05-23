29a9e203be837f5e129e2cd5b06e83ac3ac5b94d
edwinSolorzanoC <edwinuni26@gmail.com>
2024-05-23 14:29:53 -0600

    se instalo @mysql/xdevapse y se agrego busqueda en principal, cambios en footer

--- js/conexion.js
+++ js/conexion.js
@@ -3,25 +3,23 @@ let mysql = require("mysql");
 
 let conexion = mysql.createConnection({
     host: "localhost",
-    database: "dbmycolection",
     user: "root",
-    password: "patodonal26"
-});
+    password: "patodonal26",
+    database: "dbmycolection"
+})
  
-// conexion.query("SELECT * FROM tb_usuario", function(error, result, fields){
-//     if(error) throw error
-//     console.log(result)
+/*
+ conexion.query("SELECT * FROM tb_usuario", function(error, result, fields){
+     if(error) throw error
+     console.log(result)
     
-//     })
+     })
+*/
 
-  conexion.connect(function(err){
+conexion.connect(function(err){
      if(err){
-        throw err;
+        console.log(err)
     }else{
           console.log("conexion exitosa");
       }
   });
-
-//la conexion esta mal, da error. Probar en terminal de vs con, node javascript/conexion.js
-//creo que el error es de la linea 4 a las 11, pero mejjor ver video o buscar en chat....
-
